d3c065bc3b20145c4c77c4e3f21f0335594e4ffc
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2025-06-12 21:40:42 -0300

    Exibição de filmes da base de dados

--- app.js
+++ app.js
@@ -1,62 +1,29 @@
-// JSON - javascript object notation
+async function fnPegarFilmes(){
+    let filmes = await fetch ("dados-filmes.json")
+    let filmesTratados = await filmes.json()
 
-let filme = {
-    titulo: "Jurassic Park 0",
-    foto: "filme1.webp",
-    avaliacao: "10/10",
-    duracao: "1h50m"
-}
-
-console.log(filme)
-
-let filme2 = {
-    titulo: "Karate kid",
-    foto: "filme2.jpg",
-    avaliacao: "9/10",
-    duracao: "2h20m"
-}
-
-let filme3 = {
-    titulo: "Lilo e Stitch 2",
-    foto: "filme3.jpg",
-    avaliacao: "8/10",
-    duracao: "2h06m"
-}
-
-let filme4 = {
-    titulo: "Pecadores",
-    foto: "filme4.jpg",
-    avaliacao: "7/10",
-    duracao: "1h50m"
-}
-
-let filme5 = {
-    titulo: "Homem-Aranha",
-    foto: "filme5.jpg",
-    avaliacao: "10/10",
-    duracao: "2h01m"
-}
-
-function fnMontarCartao(filmeAtual){
-    document.querySelector(".lista-filmes").innerHTML += `
+    filmesTratados.slice(0,4).forEach((filmeAtual)=> {
+        document.querySelector(".lista-filmes").innerHTML += `
         <div class="card-filme">
-            <img src="img/${filmeAtual.foto}">
-            <h3>${filmeAtual.titulo}</h3>
-            <span>⭐ ${filmeAtual.avaliacao}</span>
+        <img src="${filmeAtual.foto}">
+        <h3>${filmeAtual.titulo}</h3>
+        <span> ⭐ ${filmeAtual.avaliacao}</span>
         </div>
-        `
+         `
+    })
 }
-
-//DOM - Document Object Model
-let todosOsFilmes = [filme, filme2, filme3, filme4, filme5]
-  
-todosOsFilmes.forEach((filmeAtual) => {
+ 
+ 
+fnPegarFilmes()
+ 
+function fnMontarCartao(filmeAtual) {
     document.querySelector(".lista-filmes").innerHTML += `
         <div class="card-filme">
         <img src="img/${filmeAtual.foto}">
         <h3>${filmeAtual.titulo}</h3>
-        <span>⭐ ${filmeAtual.avaliacao}</span>
+        <span> ⭐ ${filmeAtual.avaliacao}</span>
         </div>
-        `
-})
-
+         `
+}
+ 
+ 
--- app2.js
+++ app2.js
@@ -1,9 +1,18 @@
 async function fnPegarFilmes(){
     let filmes = await fetch("dados-filmes.json")
-    console.dir(filmes)
     let filmesTratados = await filmes.json()
-    console.dir(filmesTratados)
+    filmesTratados.forEach((filmeAtual)=>{
+        document.querySelector(".lista-filmes").innerHTML += `
+        <div class="card-filme">
+        <img src="${filmeAtual.foto}">
+        <h3>${filmeAtual.titulo}</h3>
+        <span>⭐ ${filmeAtual.avaliacao}</span>
+        </div>
+        `
+    })
 } 
+ 
+
 
 fnPegarFilmes()
 console.log("oi")
